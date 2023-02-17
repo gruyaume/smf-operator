92e56d392043476287e97b1bb73b59bd48d51fd1
guillaume <guillaume.belanger27@gmail.com>
2023-02-17 18:34:31 -0500

    First implementation of the smf operator

---
+++ src/charm.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# Copyright 2022 Guillaume Belanger
+# See LICENSE file for licensing details.
+
+"""Charmed operator for the 5G SMF service."""
+
+import logging
+from ipaddress import IPv4Address
+from subprocess import check_output
+from typing import Dict, Optional, Union
+
+from charms.data_platform_libs.v0.data_interfaces import DatabaseCreatedEvent, DatabaseRequires
+from charms.nrf_operator.v0.nrf import NRFAvailableEvent, NRFRequires
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from jinja2 import Environment, FileSystemLoader
+from lightkube.models.core_v1 import ServicePort
+from ops.charm import CharmBase, InstallEvent, PebbleReadyEvent
+from ops.main import main
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.pebble import Layer
+
+logger = logging.getLogger(__name__)
+
+BASE_CONFIG_PATH = "/etc/smf"
+CONFIG_FILE_NAME = "smfcfg.yaml"
+UE_ROUTING_FILE_NAME = "uerouting.conf"
+DEFAULT_DATABASE_NAME = "free5gc"
+SMF_DATABASE_NAME = "sdcore_smf"
+PFCP_PORT = 8805
+
+
+class SMFOperatorCharm(CharmBase):
+    """Main class to describe juju event handling for the 5G SMF operator."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._container_name = self._service_name = "smf"
+        self._container = self.unit.get_container(self._container_name)
+        self._database = DatabaseRequires(
+            self, relation_name="database", database_name=SMF_DATABASE_NAME
+        )
+        self._nrf_requires = NRFRequires(charm=self, relationship_name="nrf")
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.smf_pebble_ready, self._on_smf_pebble_ready)
+        self.framework.observe(self.on.database_relation_joined, self._on_smf_pebble_ready)
+        self.framework.observe(self._database.on.database_created, self._on_database_created)
+        self.framework.observe(self._nrf_requires.on.nrf_available, self._on_nrf_available)
+        self._service_patcher = KubernetesServicePatch(
+            charm=self,
+            ports=[
+                ServicePort(name="pfcp", port=PFCP_PORT, protocol="UDP"),
+                ServicePort(name="prometheus-exporter", port=9089),
+                ServicePort(name="sbi", port=29502),
+            ],
+        )
+
+    def _on_install(self, event: InstallEvent) -> None:
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
+            return
+        self._write_uerouting_config_file()
+
+    def _on_database_created(self, event: DatabaseCreatedEvent) -> None:
+        """Handle database created event."""
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
+            return
+        if not self._nrf_relation_is_created:
+            self.unit.status = BlockedStatus("Waiting for NRF relation to be created")
+            event.defer()
+            return
+        if not self._nrf_data_is_available:
+            self.unit.status = WaitingStatus("Waiting for NRF data to be available")
+            event.defer()
+            return
+        self._write_config_file(
+            default_database_url=event.uris.split(",")[0], nrf_url=self._nrf_requires.get_nrf_url()
+        )
+        self._on_smf_pebble_ready(event)
+
+    def _on_nrf_available(self, event: NRFAvailableEvent) -> None:
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
+            return
+        if not self._database_relation_is_created:
+            self.unit.status = BlockedStatus("Waiting for database relation to be created")
+            event.defer()
+            return
+        if not self._database_is_available:
+            self.unit.status = WaitingStatus("Waiting for database to be available")
+            event.defer()
+            return
+        self._write_config_file(
+            default_database_url=self._database_data["uris"].split(",")[0], nrf_url=event.url
+        )
+        self._on_smf_pebble_ready(event)
+
+    def _write_config_file(self, default_database_url: str, nrf_url: str) -> None:
+        jinja2_environment = Environment(loader=FileSystemLoader("src/templates/"))
+        template = jinja2_environment.get_template("smfcfg.yaml.j2")
+        content = template.render(
+            nrf_url=nrf_url,
+            pod_ip=self._pod_ip,
+            smf_database_name=SMF_DATABASE_NAME,
+            default_database_name=DEFAULT_DATABASE_NAME,
+            database_url=default_database_url,
+        )
+        self._container.push(path=f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}", source=content)
+        logger.info(f"Pushed {CONFIG_FILE_NAME} config file")
+
+    def _write_uerouting_config_file(self) -> None:
+        with open("src/uerouting.conf", "r") as f:
+            content = f.read()
+        self._container.push(path=f"{BASE_CONFIG_PATH}/{UE_ROUTING_FILE_NAME}", source=content)
+        logger.info(f"Pushed {UE_ROUTING_FILE_NAME} config file")
+
+    @property
+    def _nrf_data_is_available(self) -> bool:
+        """Returns whether the NRF data is available.
+
+        Returns:
+            bool: Whether the NRF data is available.
+        """
+        if not self._nrf_requires.get_nrf_url():
+            return False
+        return True
+
+    @property
+    def _database_is_available(self) -> bool:
+        """Returns whether the database is available.
+
+        Returns:
+            bool: Whether the database is available.
+        """
+        return self._database.is_resource_created()
+
+    @property
+    def _database_data(self) -> Dict:
+        """Returns the database data.
+
+        Returns:
+            Dict: The database data.
+        """
+        if not self._database_is_available:
+            raise RuntimeError("Database is not available")
+        return self._database.fetch_relation_data()[self._database.relations[0].id]
+
+    @property
+    def _config_file_is_written(self) -> bool:
+        if not self._container.exists(f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}"):
+            logger.info(f"Config file is not written: {CONFIG_FILE_NAME}")
+            return False
+        logger.info("Config file is written")
+        return True
+
+    def _on_smf_pebble_ready(
+        self, event: Union[PebbleReadyEvent, DatabaseCreatedEvent, NRFAvailableEvent]
+    ) -> None:
+        if not self._database_relation_is_created:
+            self.unit.status = BlockedStatus("Waiting for database relation to be created")
+            return
+        if not self._nrf_relation_is_created:
+            self.unit.status = BlockedStatus("Waiting for NRF relation to be created")
+            return
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
+            return
+        if not self._config_file_is_written:
+            self.unit.status = WaitingStatus("Waiting for config file to be written")
+            return
+        self._container.add_layer("smf", self._pebble_layer, combine=True)
+        self._container.replan()
+        self.unit.status = ActiveStatus()
+
+    @property
+    def _database_relation_is_created(self) -> bool:
+        return self._relation_created("database")
+
+    @property
+    def _nrf_relation_is_created(self) -> bool:
+        return self._relation_created("nrf")
+
+    def _relation_created(self, relation_name: str) -> bool:
+        """Returns whether a given Juju relation was crated.
+
+        Args:
+            relation_name (str): Relation name
+
+        Returns:
+            str: Whether the relation was created.
+        """
+        if not self.model.get_relation(relation_name):
+            return False
+        return True
+
+    @property
+    def _pebble_layer(self) -> Layer:
+        """Returns pebble layer for the charm.
+
+        Returns:
+            Layer: Pebble Layer
+        """
+        return Layer(
+            {
+                "summary": "smf layer",
+                "description": "pebble config layer for smf",
+                "services": {
+                    "smf": {
+                        "override": "replace",
+                        "startup": "enabled",
+                        "command": f"./smf --smfcfg {BASE_CONFIG_PATH}/{CONFIG_FILE_NAME} --uerouting {BASE_CONFIG_PATH}/{UE_ROUTING_FILE_NAME}",
+                        "environment": self._environment_variables,
+                    },
+                },
+            }
+        )
+
+    @property
+    def _environment_variables(self) -> dict:
+        """Returns the environment variables for the workload service."""
+        return {
+            "GRPC_GO_LOG_VERBOSITY_LEVEL": "99",
+            "GRPC_GO_LOG_SEVERITY_LEVEL": "info",
+            "GRPC_TRACE": "all",
+            "GRPC_VERBOSITY": "debug",
+            "PFCP_PORT_UPF": str(PFCP_PORT),
+            "POD_IP": str(self._pod_ip),
+        }
+
+    @property
+    def _pod_ip(self) -> Optional[IPv4Address]:
+        """Get the IP address of the Kubernetes pod."""
+        return IPv4Address(check_output(["unit-get", "private-address"]).decode().strip())
+
+
+if __name__ == "__main__":
+    main(SMFOperatorCharm)
---
+++ tests/unit/test_charm.py
@@ -0,0 +1,155 @@
+# Copyright 2022 Guillaume Belanger
+# See LICENSE file for licensing details.
+
+import unittest
+from unittest.mock import Mock, patch
+
+from ops import testing
+from ops.model import ActiveStatus
+
+from charm import SMFOperatorCharm
+
+
+class TestCharm(unittest.TestCase):
+    @patch(
+        "charm.KubernetesServicePatch",
+        lambda charm, ports: None,
+    )
+    def setUp(self):
+        self.harness = testing.Harness(SMFOperatorCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def _nrf_is_available(self) -> str:
+        nrf_url = "http://1.1.1.1"
+        nrf_relation_id = self.harness.add_relation("nrf", "nrf-operator")
+        self.harness.add_relation_unit(
+            relation_id=nrf_relation_id, remote_unit_name="nrf-operator/0"
+        )
+        self.harness.update_relation_data(
+            relation_id=nrf_relation_id, app_or_unit="nrf-operator", key_values={"url": nrf_url}
+        )
+        return nrf_url
+
+    def _database_is_available(self) -> str:
+        database_url = "http://6.6.6.6"
+        database_username = "banana"
+        database_password = "pizza"
+        database_relation_id = self.harness.add_relation("database", "mongodb")
+        self.harness.add_relation_unit(
+            relation_id=database_relation_id, remote_unit_name="mongodb/0"
+        )
+        self.harness.update_relation_data(
+            relation_id=database_relation_id,
+            app_or_unit="mongodb",
+            key_values={
+                "username": database_username,
+                "password": database_password,
+                "uris": "".join([database_url]),
+            },
+        )
+        return database_url
+
+    @patch("ops.model.Container.push")
+    def test_given_when_on_install_then_uerouting_config_file_is_written(self, patch_push):
+
+        self.harness.charm._on_install(event=Mock())
+
+
+    @patch("charm.check_output")
+    @patch("ops.model.Container.push")
+    def test_given_nrf_is_available_when_database_is_created_then_config_file_is_written(
+        self,
+        patch_push,
+        patch_check_output,
+    ):
+        pod_ip = "1.1.3.4"
+        patch_check_output.return_value = pod_ip.encode()
+        database_url_0 = "1.9.7.4:1234"
+        database_url_1 = "5.6.7.8:1111"
+        self.harness.set_can_connect(container="smf", val=True)
+
+        nrf_url = self._nrf_is_available()
+
+        self.harness.charm._on_database_created(
+            event=Mock(uris="".join([database_url_0, ",", database_url_1]))
+        )
+
+        patch_push.assert_called_with(
+            path="/etc/smf/smfcfg.yaml",
+            source=f'configuration:\n  debugProfilePort: 5001\n  enableDBStore: false\n  enableUPFAdapter: false\n  kafkaInfo:\n    brokerPort: 9092\n    brokerUri: sd-core-kafka-headless\n    topicName: sdcore-data-source-smf\n  mongodb:\n    name: free5gc\n    url: { database_url_0 }\n  nfKafka:\n    enable: false\n    topic: sdcore-nf-data-source\n    urls:\n    - sd-core-kafka-headless:9092\n  nrfUri: { nrf_url }\n  pfcp:\n    addr: { pod_ip }\n  sbi:\n    bindingIPv4: 0.0.0.0\n    port: 29502\n    registerIPv4: smf\n    scheme: http\n    tls:\n      key: gofree5gc/support/TLS/smf.key\n      pem: gofree5gc/support/TLS/smf.pem\n  serviceNameList:\n  - nsmf-pdusession\n  - nsmf-event-exposure\n  smfDBName: sdcore_smf\n  smfName: SMF\n  snssaiInfos:\n  - dnnInfos:\n    - dnn: internet\n      dns:\n        ipv4: 8.8.8.8\n        ipv6: 2001:4860:4860::8888\n      ueSubnet: 172.250.0.0/16\n    sNssai:\n      sd: "010203"\n      sst: 1\n  userplane_information:\n    links:\n    - A: gNB1\n      B: UPF\n    up_nodes:\n      UPF:\n        interfaces:\n        - endpoints:\n          - upf\n          interfaceType: N3\n          networkInstance: internet\n        node_id: upf\n        sNssaiUpfInfos:\n        - dnnUpfInfoList:\n          - dnn: internet\n          plmnId:\n            mcc: "208"\n            mnc: "93"\n          sNssai:\n            sd: "010203"\n            sst: 1\n        - dnnUpfInfoList:\n          - dnn: internet\n          plmnId:\n            mcc: "208"\n            mnc: "93"\n          sNssai:\n            sd: "112233"\n            sst: 1\n        type: UPF\n      gNB1:\n        type: AN\ninfo:\n  description: SMF initial local configuration\n  version: 1.0.0\nlogger:\n  AMF:\n    ReportCaller: false\n    debugLevel: info\n  AUSF:\n    ReportCaller: false\n    debugLevel: info\n  Aper:\n    ReportCaller: false\n    debugLevel: info\n  CommonConsumerTest:\n    ReportCaller: false\n    debugLevel: info\n  FSM:\n    ReportCaller: false\n    debugLevel: info\n  MongoDBLibrary:\n    ReportCaller: false\n    debugLevel: info\n  N3IWF:\n    ReportCaller: false\n    debugLevel: info\n  NAS:\n    ReportCaller: false\n    debugLevel: info\n  NGAP:\n    ReportCaller: false\n    debugLevel: info\n  NRF:\n    ReportCaller: false\n    debugLevel: info\n  NamfComm:\n    ReportCaller: false\n    debugLevel: info\n  NamfEventExposure:\n    ReportCaller: false\n    debugLevel: info\n  NsmfPDUSession:\n    ReportCaller: false\n    debugLevel: info\n  NudrDataRepository:\n    ReportCaller: false\n    debugLevel: info\n  OpenApi:\n    ReportCaller: false\n    debugLevel: info\n  PCF:\n    ReportCaller: false\n    debugLevel: info\n  PFCP:\n    ReportCaller: false\n    debugLevel: info\n  PathUtil:\n    ReportCaller: false\n    debugLevel: info\n  SMF:\n    ReportCaller: false\n    debugLevel: info\n  UDM:\n    ReportCaller: false\n    debugLevel: info\n  UDR:\n    ReportCaller: false\n    debugLevel: info\n  WEBUI:\n    ReportCaller: false\n    debugLevel: info',  # noqa: E501
+        )
+
+    @patch("charm.check_output")
+    @patch("ops.model.Container.push")
+    def test_given_database_is_available_when_nrf_is_available_then_config_file_is_written(
+        self,
+        patch_push,
+        patch_check_output,
+    ):
+        pod_ip = "8.7.6.5"
+        nrf_url = "2.2.2.2"
+        patch_check_output.return_value = pod_ip.encode()
+
+        self.harness.set_can_connect(container="smf", val=True)
+        database_url_0 = self._database_is_available()
+
+        self.harness.charm._on_nrf_available(event=Mock(url=nrf_url))
+
+        patch_push.assert_called_with(
+            path="/etc/smf/smfcfg.yaml",
+            source=f'configuration:\n  debugProfilePort: 5001\n  enableDBStore: false\n  enableUPFAdapter: false\n  kafkaInfo:\n    brokerPort: 9092\n    brokerUri: sd-core-kafka-headless\n    topicName: sdcore-data-source-smf\n  mongodb:\n    name: free5gc\n    url: { database_url_0 }\n  nfKafka:\n    enable: false\n    topic: sdcore-nf-data-source\n    urls:\n    - sd-core-kafka-headless:9092\n  nrfUri: { nrf_url }\n  pfcp:\n    addr: { pod_ip }\n  sbi:\n    bindingIPv4: 0.0.0.0\n    port: 29502\n    registerIPv4: smf\n    scheme: http\n    tls:\n      key: gofree5gc/support/TLS/smf.key\n      pem: gofree5gc/support/TLS/smf.pem\n  serviceNameList:\n  - nsmf-pdusession\n  - nsmf-event-exposure\n  smfDBName: sdcore_smf\n  smfName: SMF\n  snssaiInfos:\n  - dnnInfos:\n    - dnn: internet\n      dns:\n        ipv4: 8.8.8.8\n        ipv6: 2001:4860:4860::8888\n      ueSubnet: 172.250.0.0/16\n    sNssai:\n      sd: "010203"\n      sst: 1\n  userplane_information:\n    links:\n    - A: gNB1\n      B: UPF\n    up_nodes:\n      UPF:\n        interfaces:\n        - endpoints:\n          - upf\n          interfaceType: N3\n          networkInstance: internet\n        node_id: upf\n        sNssaiUpfInfos:\n        - dnnUpfInfoList:\n          - dnn: internet\n          plmnId:\n            mcc: "208"\n            mnc: "93"\n          sNssai:\n            sd: "010203"\n            sst: 1\n        - dnnUpfInfoList:\n          - dnn: internet\n          plmnId:\n            mcc: "208"\n            mnc: "93"\n          sNssai:\n            sd: "112233"\n            sst: 1\n        type: UPF\n      gNB1:\n        type: AN\ninfo:\n  description: SMF initial local configuration\n  version: 1.0.0\nlogger:\n  AMF:\n    ReportCaller: false\n    debugLevel: info\n  AUSF:\n    ReportCaller: false\n    debugLevel: info\n  Aper:\n    ReportCaller: false\n    debugLevel: info\n  CommonConsumerTest:\n    ReportCaller: false\n    debugLevel: info\n  FSM:\n    ReportCaller: false\n    debugLevel: info\n  MongoDBLibrary:\n    ReportCaller: false\n    debugLevel: info\n  N3IWF:\n    ReportCaller: false\n    debugLevel: info\n  NAS:\n    ReportCaller: false\n    debugLevel: info\n  NGAP:\n    ReportCaller: false\n    debugLevel: info\n  NRF:\n    ReportCaller: false\n    debugLevel: info\n  NamfComm:\n    ReportCaller: false\n    debugLevel: info\n  NamfEventExposure:\n    ReportCaller: false\n    debugLevel: info\n  NsmfPDUSession:\n    ReportCaller: false\n    debugLevel: info\n  NudrDataRepository:\n    ReportCaller: false\n    debugLevel: info\n  OpenApi:\n    ReportCaller: false\n    debugLevel: info\n  PCF:\n    ReportCaller: false\n    debugLevel: info\n  PFCP:\n    ReportCaller: false\n    debugLevel: info\n  PathUtil:\n    ReportCaller: false\n    debugLevel: info\n  SMF:\n    ReportCaller: false\n    debugLevel: info\n  UDM:\n    ReportCaller: false\n    debugLevel: info\n  UDR:\n    ReportCaller: false\n    debugLevel: info\n  WEBUI:\n    ReportCaller: false\n    debugLevel: info',
+        )
+
+    @patch("charm.check_output")
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_is_written_when_pebble_ready_then_pebble_plan_is_applied(
+        self,
+        patch_exists,
+        patch_check_output,
+    ):
+        pod_ip = "1.1.1.1"
+        patch_exists.return_value = True
+        patch_check_output.return_value = pod_ip.encode()
+
+        self._database_is_available()
+        self._nrf_is_available()
+
+        self.harness.container_pebble_ready(container_name="smf")
+
+        expected_plan = {
+            "services": {
+                "smf": {
+                    "override": "replace",
+                    "command": "./smf --smfcfg /etc/smf/smfcfg.yaml",
+                    "startup": "enabled",
+                    "environment": {
+                        "GRPC_GO_LOG_VERBOSITY_LEVEL": "99",
+                        "GRPC_GO_LOG_SEVERITY_LEVEL": "info",
+                        "GRPC_TRACE": "all",
+                        "GRPC_VERBOSITY": "debug",
+                        "PFCP_PORT_UPF": "8805",
+                        "POD_IP": pod_ip,
+                    },
+                }
+            },
+        }
+
+        updated_plan = self.harness.get_container_pebble_plan("smf").to_dict()
+
+        self.assertEqual(expected_plan, updated_plan)
+
+    @patch("charm.check_output")
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_is_written_when_pebble_ready_then_status_is_active(
+        self, patch_exists, patch_check_output
+    ):
+        patch_exists.return_value = True
+        patch_check_output.return_value = b"1.2.3.4"
+
+        self._database_is_available()
+        self._nrf_is_available()
+
+        self.harness.container_pebble_ready("smf")
+
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
